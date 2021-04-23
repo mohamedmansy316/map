34f6f421ed469ac9167d9fa33f076aa15b813319
Mohamed Mansy <52934889+mohamedmansy316@users.noreply.github.com>
2021-04-23 02:12:54 +0200

    add

--- js/script.js
+++ js/script.js
@@ -3,122 +3,122 @@ $(document).ready(function () {
 
   $(".1Angola ,.Angola").click(function () {
     $(".tippy").hide();
-    $(".angola").show(200);
+    $(".angola").fadeToggle(200);
   });
   $(".1Green-head , .Green-head").click(function () {
     $(".tippy").hide();
-    $(".green-head").show(200);
+    $(".green-head").fadeToggle(200);
   });
   $(".1Ginia , .Ginia").click(function () {
     $(".tippy").hide();
-    $(".ginia").show(200);
+    $(".ginia").fadeToggle(200);
   });
   $(".1Muzambik , .Muzambik").click(function () {
     $(".tippy").hide();
-    $(".muzambik").show(200);
+    $(".muzambik").fadeToggle(200);
   });
   $(".1Kongho , .Kongho").click(function () {
     $(".tippy").hide();
-    $(".kongho").show(200);
+    $(".kongho").fadeToggle(200);
   });
   $(".1Big-kongho , .Big-kongho").click(function () {
     $(".tippy").hide();
-    $(".big-kongho").show(200);
+    $(".big-kongho").fadeToggle(200);
   });
   $(".1Banin , .Banin").click(function () {
     $(".tippy").hide();
-    $(".banin").show(200);
+    $(".banin").fadeToggle(200);
   });
   $(".1Kameron , .Kameron").click(function () {
     $(".tippy").hide();
-    $(".kameron").show(200);
+    $(".kameron").fadeToggle(200);
   });
   $(".1Togo ,.Togo").click(function () {
     $(".tippy").hide();
-    $(".togo").show(200);
+    $(".togo").fadeToggle(200);
   });
   $(".1Haity, .Haity").click(function () {
     $(".tippy").hide();
-    $(".haity").show(200);
+    $(".haity").fadeToggle(200);
   });
   $(".1Kotivoar , .Kotivoar").click(function () {
     $(".tippy").hide();
-    $(".kotivoar").show(200);
+    $(".kotivoar").fadeToggle(200);
   });
   $(".1France , .France").click(function () {
     $(".tippy").hide();
-    $(".france").show(200);
+    $(".france").fadeToggle(200);
   });
   $(".1America , .America").click(function () {
     $(".tippy").hide();
-    $(".america").show(200);
+    $(".america").fadeToggle(200);
   });
   $(".1Brazil , .Brazil").click(function () {
     $(".tippy").hide();
-    $(".brazil").show(200);
+    $(".brazil").fadeToggle(200);
   });
   $(".1Venezuela , .Venezuela").click(function () {
     $(".tippy").hide();
-    $(".venezuela").show(200);
+    $(".venezuela").fadeToggle(200);
   });
   $(".1Nicaragua , .Nicaragua").click(function () {
     $(".tippy").hide();
-    $(".nicaragua").show(200);
+    $(".nicaragua").fadeToggle(200);
   });
   $(".1Guatemala , .Guatemala").click(function () {
     $(".tippy").hide();
-    $(".guatemala").show(200);
+    $(".guatemala").fadeToggle(200);
   });
   $(".1Colombia , .Colombia").click(function () {
     $(".tippy").hide();
-    $(".colombia").show(200);
+    $(".colombia").fadeToggle(200);
   });
   $(".1Paraguay , .Paraguay").click(function () {
     $(".tippy").hide();
-    $(".paraguay").show(200);
+    $(".paraguay").fadeToggle(200);
   });
   $(".1Mexico , .Mexico").click(function () {
     $(".tippy").hide();
-    $(".mexico").show(200);
+    $(".mexico").fadeToggle(200);
   });
   $(".1Peru , .Peru").click(function () {
     $(".tippy").hide();
-    $(".peru").show(200);
+    $(".peru").fadeToggle(200);
   });
   $(".1Honduras , .Honduras").click(function () {
     $(".tippy").hide();
-    $(".honduras").show(200);
+    $(".honduras").fadeToggle(200);
   });
   $(".1Dominicans , .Dominicans").click(function () {
     $(".tippy").hide();
-    $(".dominicans").show(200);
+    $(".dominicans").fadeToggle(200);
   });
   $(".1Argentina , .Argentina").click(function () {
     $(".tippy").hide();
-    $(".argentina").show(200);
+    $(".argentina").fadeToggle(200);
   });
   $(".1Equatorial-Guinea , .Equatorial-Guinea").click(function () {
     $(".tippy").hide();
-    $(".equatorial-Guinea").show(200);
+    $(".equatorial-Guinea").fadeToggle(200);
   });
   $(".1Salvador , .Salvador").click(function () {
     $(".tippy").hide();
-    $(".salvador").show(200);
+    $(".salvador").fadeToggle(200);
   });
   $(".1Costarica , .Costarica").click(function () {
     $(".tippy").hide();
-    $(".costarica").show(200);
+    $(".costarica").fadeToggle(200);
   });
   $(".1Ecuador , .Ecuador").click(function () {
     $(".tippy").hide();
-    $(".ecuador").show(200);
+    $(".ecuador").fadeToggle(200);
   });
   $(".1Bolivia , .Bolivia").click(function () {
     $(".tippy").hide();
-    $(".bolivia").show(200);
+    $(".bolivia").fadeToggle(200);
   });
   $(".1Urgawi , .Urgawi").click(function () {
     $(".tippy").hide();
-    $(".urgawi").show(200);
+    $(".urgawi").fadeToggle(200);
   });
 });
